bac695dcb0aa20507fef096597757b15acf32b68
hiragi <sasanoha31@gmail.com>
2012-10-19 03:59:53 +0900

    added quicksort2

--- quicksort.lisp
+++ quicksort.lisp
@@ -15,3 +15,14 @@
 		(bunkaku (cdr lst) pv lst1 (cons a lst2))))))
 
 (qsort '(8 6 5 1 9 2))
+
+
+;; No.2
+(defun quick-sort (f l)
+  (unless (atom l)
+	(let ((p (car l)) l1 l2)
+	  (dolist (n (cdr l))
+		(if (funcall f n p) (push n l1) (push n l2)))
+	  (append (quick-sort f l1) (cons p (quick-sort f l2))))))
+
+(quick-sort #'< '( 8 6 5 1 9 2))
